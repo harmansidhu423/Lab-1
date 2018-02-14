b3ff2bcfedf29f33033dbd535d02d3bd51f484b3
Your Name <harmansidhu422@gmail.com>
2018-02-13 23:28:35 -0500

    Initial

---
+++ Lab 1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab_1.Startup))]
+namespace Lab_1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
